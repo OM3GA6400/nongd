f7f622a13a737c6bd32f0bfd25ecc515d76159fb
Fleeym <61891787+Fleeym@users.noreply.github.com>
2023-10-04 15:23:02 +0300

    fix scaling issues on NongAddPopup

--- src/UI/NongAddPopup.cpp
+++ src/UI/NongAddPopup.cpp
@@ -3,6 +3,9 @@
 bool NongAddPopup::setup(NongPopup* parent) {
     this->setTitle("Add NONG");
     this->m_parentPopup = parent;
+
+    auto center = CCDirector::sharedDirector()->getWinSize() / 2;
+
     this->m_selectSongButton = CCMenuItemSpriteExtra::create(
         ButtonSprite::create("Select mp3..."),
         this,
@@ -12,7 +15,7 @@ bool NongAddPopup::setup(NongPopup* parent) {
     this->m_selectSongMenu->setID("select-file-menu");
     this->m_selectSongButton->setID("select-file-button");
     this->m_selectSongMenu->addChild(this->m_selectSongButton);
-    this->m_selectSongMenu->setPosition(ccp(284.5f, 200.f));
+    this->m_selectSongMenu->setPosition(center.width, center.height + this->getPopupSize().height / 2 - 75.f);
 
     this->m_addSongButton = CCMenuItemSpriteExtra::create(
         ButtonSprite::create("Add"),
@@ -23,7 +26,7 @@ bool NongAddPopup::setup(NongPopup* parent) {
     this->m_addSongMenu->setID("add-song-menu");
     this->m_addSongButton->setID("add-song-button");
     this->m_addSongMenu->addChild(this->m_addSongButton);
-    this->m_addSongMenu->setPosition(ccp(284.5f, 67.f));
+    this->m_addSongMenu->setPosition(center.width, center.height - this->getPopupSize().height / 2 + 25.f);
 
     this->m_containerLayer = CCLayer::create();
 
@@ -66,6 +69,7 @@ void NongAddPopup::openFile(CCObject* target) {
 }
 
 void NongAddPopup::createSelectedSongLabel(const std::string& label) {
+    
     if (auto label = this->m_containerLayer->getChildByID("selected-song-label")) {
         this->m_containerLayer->removeChild(label);
     }
@@ -73,7 +77,8 @@ void NongAddPopup::createSelectedSongLabel(const std::string& label) {
     this->m_selectedSongPath = CCLabelBMFont::create(label.c_str(), "goldFont.fnt");
     this->m_selectedSongPath->setID("selected-song-label");
     this->m_selectedSongPath->setScale(0.5f);
-    this->m_selectedSongPath->setPosition(ccp(284.5f, 233.f));
+    auto center = CCDirector::sharedDirector()->getWinSize() / 2;
+    this->m_selectedSongPath->setPosition(center.width, center.height + this->getPopupSize().height / 2 - 45.f);
     this->m_selectedSongPath->limitLabelWidth(300.f, 0.6f, 0.1f);
     this->m_selectedSongPath->setLineBreakWithoutSpace(true);
 
@@ -81,19 +86,20 @@ void NongAddPopup::createSelectedSongLabel(const std::string& label) {
 }
 
 void NongAddPopup::createInputs() {
+    auto centered = CCDirector::sharedDirector()->getWinSize() / 2;
     auto bgSprite = CCScale9Sprite::create(
         "square02b_001.png", { 0.0f, 0.0f, 80.0f, 80.0f }
     );
     bgSprite->setScale(0.7f);
     bgSprite->setColor({ 0, 0, 0 });
     bgSprite->setOpacity(75);
-    bgSprite->setPosition(ccp(284.5f, 153.f));
+    bgSprite->setPosition(centered);
     bgSprite->setContentSize({ 409.f, 54.f });
     bgSprite->setID("song-name-bg");
 
-    this->m_songNameInput = CCTextInputNode::create(250.f, 20.f, "Song name", "bigFont.fnt");
+    this->m_songNameInput = CCTextInputNode::create(250.f, 30.f, "Song name", "bigFont.fnt");
     this->m_songNameInput->setID("song-name-input");
-    this->m_songNameInput->setPosition(ccp(284.5f, 155.f));
+    this->m_songNameInput->setPosition(centered);
     this->m_songNameInput->ignoreAnchorPointForPosition(false);
     this->m_songNameInput->m_textField->setAnchorPoint({ 0.5f, 0.5f });
     this->m_songNameInput->m_placeholderLabel->setAnchorPoint({ 0.5f, 0.5f });
@@ -107,7 +113,7 @@ void NongAddPopup::createInputs() {
 
     this->m_artistNameInput = CCTextInputNode::create(250.f, 20.f, "Artist name", "bigFont.fnt");
     this->m_artistNameInput->setID("artist-name-input");
-    this->m_artistNameInput->setPosition(ccp(284.5f, 110.f));
+    this->m_artistNameInput->setPosition(centered.width, centered.height - 50.f);
     this->m_artistNameInput->ignoreAnchorPointForPosition(false);
     this->m_artistNameInput->setMaxLabelScale(0.7f);
     this->m_artistNameInput->m_textField->setAnchorPoint({ 0.5f, 0.5f });
@@ -120,7 +126,7 @@ void NongAddPopup::createInputs() {
     bgSprite_artist->setScale(0.7f);
     bgSprite_artist->setColor({ 0, 0, 0 });
     bgSprite_artist->setOpacity(75);
-    bgSprite_artist->setPosition(ccp(284.5f, 108.f));
+    bgSprite_artist->setPosition(centered.width, centered.height - 50.f);
     bgSprite_artist->setContentSize({ 409.f, 54.f });
     bgSprite_artist->setID("artist-name-bg");
 
